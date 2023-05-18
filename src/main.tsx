77394b3c6f508957464aef02a220669ffa26de86
Long Tran <longntran48@gmail.com>
2023-05-18 16:03:27 +0700

    update: i18nnext config

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-import './i18n';
+import './i18n/config';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
