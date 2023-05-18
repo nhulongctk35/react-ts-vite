3018f0a99303902e4e91a31eb646cfd62b4b7616
Long Tran <longntran48@gmail.com>
2023-05-18 09:30:27 +0700

    init: create initial project

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+  },
+}
